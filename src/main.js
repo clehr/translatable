57ec77d7b26f3a79370e0d6a8c711ddb9061c8ff
Christian Lehr <christian_lehr.1@gmx.de>
2019-04-27 12:19:58 +0200

    setup project with VueSax

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+Vue.use(Vuesax)
 
 Vue.config.productionTip = false
 
