f507472142b67c9756ab07dbccd8594febc293c4
Christian Lehr <christian_lehr.1@gmx.de>
2019-04-27 20:11:57 +0200

    layout

--- src/components/PDFExport.vue
+++ src/components/PDFExport.vue
@@ -25,31 +25,5 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h3 {
-        margin: 40px 0 0;
-    }
-
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-
-    a {
-        color: #42b983;
-    }
-
-    div {
-        margin-left: auto;
-        margin-right: auto;
-    }
-
-    p {
-        margin: 10px 10px 10px 10px;
-    }
 
 </style>
