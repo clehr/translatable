a55dc0aeb2442bf2e46de96270e109c3d231956a
Christian Lehr <christian_lehr.1@gmx.de>
2019-04-27 22:20:09 +0200

    add validation for word to translate

--- src/components/Translator.vue
+++ src/components/Translator.vue
@@ -37,9 +37,9 @@
         </vs-row>
 
         <vs-row class="shorten">
-            <vs-input :success="this.textToTranslate !== ''" :danger="this.textToTranslate === ''" danger-text="Enter the word you want to translate"
+            <vs-input :success="this.textToTranslate !== '' && this.sourceLanguage !== 'word to translate'" :danger="this.textToTranslate === '' || this.sourceLanguage === 'word to translate'" danger-text="Enter the word you want to translate and select a language"
                       :label-placeholder=sourceLanguage v-model="textToTranslate" @keyup.enter="translate"/>
-            <vs-input :label-placeholder=targetLanguage v-model="translatedText" @keyup.enter="translate"/>
+            <vs-input :danger="this.targetLanguage === 'translated word'" danger-text="Select a language" :label-placeholder=targetLanguage v-model="translatedText" @keyup.enter="translate"/>
         </vs-row>
 
         <vs-button class="margin-top" v-on:click="translate">Translate (Enter)</vs-button>
