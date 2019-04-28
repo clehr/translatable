65622b4a32df8767974a0426877778249ff55174
Christian Lehr <christian_lehr.1@gmx.de>
2019-04-28 10:42:08 +0200

    Revert "try vue touch"
    This reverts commit db6b7e2d

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,25 +1,12 @@
-<template xmlns:v-touch="http://www.w3.org/1999/xhtml">
+<template>
     <div>
-        <h2>{{tapText}}</h2>
-
-        <span v-touch:tap="tapHandler">Tap Me</span>
-
+        <h2>Welcome to Translatable!</h2>
     </div>
 </template>
 
 <script>
     export default {
         name: 'Heading',
-        data() {
-            return {
-                tapText: 'Welcome'
-            }
-        },
-        methods: {
-            tapHandler() {
-                this.tapText = 'Tap worked'
-            }
-        }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuesax from 'vuesax'
-import Vue2TouchEvents from 'vue2-touch-events'
 
 import 'vuesax/dist/vuesax.css' //Vuesax styles
 import 'material-icons/iconfont/material-icons.css';
-Vue.use(Vuesax, Vue2TouchEvents)
+Vue.use(Vuesax)
 
 Vue.config.productionTip = false
 
